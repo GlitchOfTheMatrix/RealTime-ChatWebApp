1e6e412e5803892381c6ef51f28ac68536404442
Ryan Hans <primaryryanhans@gmail.com>
2026-02-21 18:43:07 +0530

    Added updateProfile method contoller

--- BackEnd/src/controllers/auth.controller.js
+++ BackEnd/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -82,5 +84,26 @@ export const logout = (req, res) => {
   } catch (error) {
     console.log("Error in logout controller: ", error);
     res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile Picture is required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findById(userId, {
+      profilePic: uploadResponse.secure_url,
+      new: true,
+    });
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in Update Profile Controller: ", error);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
