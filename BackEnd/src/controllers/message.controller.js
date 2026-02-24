5cc2ea2e34b3d69a21bf45f8b314f1ba5758ecb7
Ryan Hans <primaryryanhans@gmail.com>
2026-02-24 22:48:59 +0530

    Added sendMessage Function

--- BackEnd/src/controllers/message.controller.js
+++ BackEnd/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -35,6 +36,38 @@ export const getMessages = async (req, res) => {
     res.status(200).json(messages);
   } catch (error) {
     console.log("Error in getMessages Controller: ", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+
+    if (image) {
+      // uploading base64 to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // todo: realtime functionality goes here => socket.io
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage Controller: ", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
